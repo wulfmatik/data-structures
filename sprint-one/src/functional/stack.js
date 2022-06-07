b1e01453e305991b727b857f45f55278e929f113
Blake Crawford <blake.crawford@outlook.com>
2022-06-06 18:28:44 -0700

    added push pop and size to stack functionality

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,32 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    // create array of numeric keys
+    var storageLength = Object.keys(storage).length;
+    // store values at numeric keys inside storage object
+    storage[storageLength] = value;
+
   };
 
   someInstance.pop = function() {
+    // create array of numeric keys currently in storage
+    var storageLength = Object.keys(storage).length;
+    //  if storageLength has values in it
+    if (storageLength > 0) {
+      // create popped value equal to last property in storage object
+      var poppedValue = storage[storageLength - 1];
+      // delete last property in object
+      delete storage[storageLength - 1];
+      // return poppedValue
+      return poppedValue;
+    }
   };
 
   someInstance.size = function() {
+    // create storageArray to keep track of number of keys in storage
+    var storageArray = Object.keys(storage);
+    // return the length of the storage array
+    return storageArray.length;
   };
 
   return someInstance;
