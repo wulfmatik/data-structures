bc3d3fc22ff98638a93ef7f1f277fa034e842712
Blake Crawford <blake.crawford@outlook.com>
2022-06-07 23:37:18 -0700

    Complete pseudoclassical stack and queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.counter = 0;
 };
+
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.counter] = value;
+  this.counter++;
+};
+
+Queue.prototype.dequeue = function() {
+  var storageLength = Object.keys(this.storage).length;
+
+  if (storageLength > 0) {
+    var dequeValue = this.storage[Object.keys(this.storage).shift()];
+    delete this.storage[Object.keys(this.storage).shift()];
+    return dequeValue;
+  }
+};
+
+Queue.prototype.size = function() {
+  return Object.keys(this.storage).length;
+};
+
+var SomeInstance = new Queue();
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
 };
+
+Stack.prototype.push = function(value) {
+  var storageLength = Object.keys(this.storage).length;
+  this.storage[storageLength] = value;
+};
+
+Stack.prototype.pop = function() {
+  var storageLength = Object.keys(this.storage).length;
+
+  if (storageLength > 0) {
+    var poppedValue = this.storage[storageLength - 1];
+    delete this.storage[storageLength - 1];
+    return poppedValue;
+  }
+};
+
+Stack.prototype.size = function() {
+  return Object.keys(this.storage).length;
+};
+
+var SomeInstance = new Stack();
 
 
