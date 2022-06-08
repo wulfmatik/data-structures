a17ab70d18440e40a4855c708772845b8734c591
Blake Crawford <blake.crawford@outlook.com>
2022-06-08 10:14:12 -0700

    Complete sprint one

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,29 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.counter = 0;
+  }
+
+  enqueue(value) {
+    this.storage[this.counter] = value;
+    this.counter++;
+  }
+
+  dequeue() {
+    var storageLength = Object.keys(this.storage).length;
+
+    if (storageLength > 0) {
+      var dequeValue = this.storage[Object.keys(this.storage).shift()];
+      delete this.storage[Object.keys(this.storage).shift()];
+      return dequeValue;
+    }
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
   }
 
 }
+
+var someInstance = new Queue;
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,28 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+  }
+
+  push(value) {
+    var storageLength = Object.keys(this.storage).length;
+    this.storage[storageLength] = value;
+  }
+
+  pop() {
+    var storageLength = Object.keys(this.storage).length;
+
+    if (storageLength > 0) {
+      var poppedValue = this.storage[storageLength - 1];
+      delete this.storage[storageLength - 1];
+      return poppedValue;
+    }
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
   }
 
 }
+
+var someInstance = new Stack();
