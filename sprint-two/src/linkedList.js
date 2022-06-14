78fc0ffa8a5e554fe1785825a4cb698250a648a2
Blake Crawford <blake.crawford@outlook.com>
2022-06-13 20:18:32 -0700

    Complete Linked List

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,43 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if (list.head === null) {
+      list.head = Node(value);
+    }
+
+    if (list.head !== null) {
+      list.head.next = Node(value);
+    }
+
+    list.tail = Node(value);
   };
 
   list.removeHead = function() {
+    var removedHead = list.head;
+    delete list.head;
+    if (removedHead.next !== null) {
+      list.head = removedHead.next;
+    }
+    return removedHead.value;
   };
 
   list.contains = function(target) {
+    var result = false;
+    var currentNode = list.head;
+
+    while (currentNode !== null) {
+      //console.log(currentNode);
+      if (currentNode.value === target) {
+        result = true;
+        break;
+      }
+
+      currentNode = currentNode.next;
+      console.log(currentNode);
+    }
+    //console.log(result)
+    return result;
+
   };
 
   return list;
