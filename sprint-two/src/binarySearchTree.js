c8b8e3fbf233757867e5f97a4d4a4da5b204672a
Blake Crawford <blake.crawford@outlook.com>
2022-06-18 17:17:04 -0700

    Complete Binary Search Tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,67 @@
 var BinarySearchTree = function(value) {
+
+  var newNode = {};
+
+  _.extend(newNode, nodeMethods);
+
+  newNode.value = value;
+
+  newNode.left = null;
+  newNode.right = null;
+
+  return newNode;
+
+};
+
+var nodeMethods = {};
+
+
+nodeMethods.insert = function(value) {
+
+  if (this.value < value) {
+    if (this.right === null) {
+      this.right = BinarySearchTree(value);
+      return;
+    }
+    this.right.insert(value);
+  } else if (this.value > value) {
+    if (this.left === null) {
+      this.left = BinarySearchTree(value);
+      return;
+    }
+    this.left.insert(value);
+  }
+
+};
+
+nodeMethods.contains = function(value) {
+
+  if (this.value === value) {
+    return true;
+  }
+  if (this.left === null && this.right === null && this.value !== value) {
+    return false;
+  }
+  if (this.left.value >= value && this.left !== null) {
+    return this.left.contains(value);
+  } else if (this.right.value <= value && this.right !== null) {
+    return this.right.contains(value);
+  }
+
+};
+
+nodeMethods.depthFirstLog = function(func) {
+
+  func(this.value);
+
+  if (this.left !== null) {
+    this.left.depthFirstLog(func);
+  }
+
+  if (this.right !== null) {
+    this.right.depthFirstLog(func);
+  }
+
 };
 
 
